c9a896ad2c8e03a40d02ef6c463cdf34fe716f10
PavelKeuzerov <pit23111987@gmail.com>
2024-03-15 10:54:09 +0300

    rework 2 task

--- AQA_Java/src/main/java/lesson_8/PhoneBook.java
+++ AQA_Java/src/main/java/lesson_8/PhoneBook.java
@@ -9,10 +9,14 @@ public class PhoneBook {
         phoneBook.add("dog", "222-3333");
         phoneBook.add("ant", "333-444-5555");
         phoneBook.add("ant", "444-5555");
+        phoneBook.add("rat", "444-5555");
+
 
         System.out.println("Phonebook");
         System.out.println("Ant number: " + phoneBook.get("ant"));
         System.out.println("Dog number: " + phoneBook.get("dog"));
+        System.out.println("Rat number: " + phoneBook.get("rat"));
+
     }
 
     private final Map<String, List<String>> phoneBook = new HashMap<>();
