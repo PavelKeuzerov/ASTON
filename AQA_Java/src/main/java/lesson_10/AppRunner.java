8213e5ea9f5ed47f953cd08f6e36d410f712b835
PavelKeuzerov <pit23111987@gmail.com>
2024-03-16 22:47:21 +0300

    completed all

--- AQA_Java/src/main/java/lesson_10/AppRunner.java
+++ AQA_Java/src/main/java/lesson_10/AppRunner.java
@@ -2,54 +2,33 @@ package lesson_10;
 
 public class AppRunner {
     public static void main(String[] args) {
-        //Инициализируем объекты
-        Apple a1 = new Apple(2);
-        Apple a2 = new Apple(3);
-        Apple a3 = new Apple(4);
-        Apple a4 = new Apple(2);
-        Apple a5 = new Apple(3);
-        Orange o1 = new Orange(55);
-        Orange o2 = new Orange(3);
-        //Создаем коробки
+        Apple apple1 = new Apple(2);
+        Apple apple2 = new Apple(3);
+        Apple apple3 = new Apple(4);
+        Apple apple4 = new Apple(5);
+        Orange orange1 = new Orange(55);
+        Orange orange2 = new Orange(3);
+
         Box<Apple> appleBox = new Box<>();
         Box<Apple> appleBox1 = new Box<>();
         Box<Orange> orangeBox = new Box<>();
-        //Заполняем коробки
-        orangeBox.add(o1);
-        orangeBox.add(o2);
-        appleBox.add(a1);
-        appleBox.add(a2);
-        appleBox.add(a3);
-        appleBox1.add(a4);
-        appleBox1.add(a5);
-        //Результаты
-        System.out.println("appleBox: " + appleBox.getWeight());
+
+        appleBox.add(apple1);
+        appleBox.add(apple2);
+        appleBox1.add(apple3);
+        appleBox1.add(apple4);
+        orangeBox.add(orange1);
+        orangeBox.add(orange2);
+
         System.out.println("orangeBox: " + orangeBox.getWeight());
+        System.out.println("appleBox: " + appleBox.getWeight());
         System.out.println("appleBox1: " + appleBox1.getWeight());
-//        System.out.print("Compare: ");
-//        System.out.println(appleBox.compare(orangeBox));
-//        System.out.println(appleBox.compare(appleBox));
-//
 
+        System.out.println(appleBox.compare(orangeBox));
+        System.out.println(appleBox.compare(appleBox));
 
-
-
-//        System.out.println("***Before union boxes***");
-//        System.out.println("appleBox:");
-//        System.out.println(appleBox.getFruitInBox());
-//        System.out.println("appleBox1:");
-//        System.out.println(appleBox1.getFruitInBox());
-//        System.out.println("*Union*");
         appleBox.pourTo(appleBox1);
-
-//        orangeBox.unionFruitBox(appleBox1); - так нельзя <Orange> не будет сравнивать с <Apple>
-//        orangeBox.pourTo(orangeBox);
-//        System.out.println("appleBox:");
-//        System.out.println(appleBox.getFruitInBox());
-//        System.out.println("appleBox1:");
-//        System.out.println(appleBox1.getFruitInBox());
-//        System.out.print("appleBox.getBoxWeight() after union: ");
-        System.out.println(appleBox.getWeight());
-        System.out.println(appleBox1.getWeight());
+        System.out.println("after moving: " + appleBox.getWeight() + " appleBox");
+        System.out.println("after moving: " + appleBox1.getWeight() + " appleBox1");
     }
 }
