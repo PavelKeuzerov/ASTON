faeffe28f08bb39842c76f1fca7e62d15eaf174a
PavelKeuzerov <pit23111987@gmail.com>
2024-03-23 22:28:35 +0300

    rw checked

--- AQA_Java/src/main/java/Factorial.java
+++ AQA_Java/src/main/java/Factorial.java
@@ -4,6 +4,9 @@ public class Factorial {
     }
 
     public static int calculateFactorial(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Number must be non-negative.");
+        }
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result = result * i;
