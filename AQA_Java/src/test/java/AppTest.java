32e06a6eca965e283b329f9b3e1f1719f57dd1ff
PavelKeuzerov <pit23111987@gmail.com>
2024-03-29 22:09:04 +0300

    finaly version

--- AQA_Java/src/test/java/AppTest.java
+++ AQA_Java/src/test/java/AppTest.java
@@ -1,29 +1,25 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
 
 public class AppTest {
-
     WebDriver driver;
     WebDriverWait webDriverWait;
 
-    @BeforeAll
-    static void setupDriver() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
+    @BeforeMethod
     void initBrowser() {
         driver = new ChromeDriver();
-        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5000));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get("https://www.mts.by/");
         if (driver.findElement(By.id("cookie-agree")).isDisplayed()) {
             driver.findElement(By.id("cookie-agree")).click();
@@ -31,30 +27,30 @@ public class AppTest {
     }
 
     @Test
-    void BlockCheck() {
+    public void BlockCheck() {
         String onlinePaymentTitle = driver.findElement(By.xpath("//h2[contains(.,'Онлайн пополнение')]"))
                 .getText().replaceAll("\n", " ");
-        Assertions.assertEquals("Онлайн пополнение без комиссии", onlinePaymentTitle);
+        Assert.assertEquals("Онлайн пополнение без комиссии", onlinePaymentTitle);
     }
 
     @Test
-    void CheckingLogos() {
+    public void CheckingLogos() {
         List<WebElement> elementList = driver.findElements(By.xpath("//img[contains(@src, '/pay')]"));
         for (WebElement we :
                 elementList) {
-            Assertions.assertTrue(we.isDisplayed());
+            Assert.assertTrue(we.isDisplayed());
         }
     }
 
     @Test
-    void CheckMoreAboutServices() {
+    public void CheckMoreAboutServices() {
         String expectedURL = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
         driver.findElement(By.xpath("//a[contains(.,'Подробнее о сервисе')]")).click();
-        Assertions.assertEquals(expectedURL, driver.getCurrentUrl());
+        Assert.assertEquals(expectedURL, driver.getCurrentUrl());
     }
 
     @Test
-    void CheckingContinueButton() {
+    public void CheckingContinueButton() throws InterruptedException {
         driver.findElement(By.id("connection-phone")).sendKeys("297777777");
         driver.findElement(By.id("connection-sum")).sendKeys("2");
         driver.findElement(By.xpath("//button[contains(.,'Продолжить')][1]")).click();
@@ -63,11 +59,11 @@ public class AppTest {
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bepaid-iframe']")));
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//div[@class='header__payment']")));
-        Assertions.assertTrue(driver.findElement(By.xpath("//p[@class='header__payment-info']")).getText()
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@class='header__payment-info']")).getText()
                 .contains("Оплата: Услуги связи Номер:"));
     }
 
-    @AfterEach
+    @AfterMethod
     void tearDown() {
         driver.quit();
     }
