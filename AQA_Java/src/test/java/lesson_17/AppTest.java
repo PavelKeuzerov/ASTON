45f6d87484f06efae336aa3bcd26b718303cff3e
PavelKeuzerov <pit23111987@gmail.com>
2024-04-10 10:32:16 +0300

    create projeckt

---
+++ AQA_Java/src/test/java/lesson_17/AppTest.java
@@ -0,0 +1,31 @@
+package lesson_17;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppTest {
+    static AppiumDriver<AndroidElement> driver = null;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "HUAWEI MAR-LX1M API 29");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
+        //  capabilities.setCapability(MobileCapabilityType.NO_RESET, true); - из-за этого не работает
+
+
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+}
