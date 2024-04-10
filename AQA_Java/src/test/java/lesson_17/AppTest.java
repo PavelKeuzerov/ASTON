26314c0fb020081a306c0ed830bace465612862f
PavelKeuzerov <pit23111987@gmail.com>
2024-04-10 22:33:56 +0300

    add lesson_17 and video

--- AQA_Java/src/test/java/lesson_17/AppTest.java
+++ AQA_Java/src/test/java/lesson_17/AppTest.java
@@ -1,31 +1,47 @@
 package lesson_17;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
+public class AppTest extends BaseTest {
+    @Test(description = "addition test")
+    public void additionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"сложить\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "12");
+    }
 
-public class AppTest {
-    static AppiumDriver<AndroidElement> driver = null;
+    @Test(description = "subtraction test")
+    public void subtractionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"вычесть\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "6");
+    }
 
-    @BeforeTest
-    public void setup() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "HUAWEI MAR-LX1M API 29");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
-        //  capabilities.setCapability(MobileCapabilityType.NO_RESET, true); - из-за этого не работает
+    @Test(description = "multiplication test")
+    public void multiplicationTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"умножение\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "27");
+    }
 
-
-            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    @Test(description = "division test")
+    public void divisionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"разделить\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "3");
     }
 }
