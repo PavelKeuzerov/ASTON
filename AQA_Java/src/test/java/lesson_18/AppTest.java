3bc47da49a32761faf782871244829ecaf0ec135
PavelKeuzerov <pit23111987@gmail.com>
2024-04-16 00:11:08 +0300

    add allure

---
+++ AQA_Java/src/test/java/lesson_18/AppTest.java
@@ -0,0 +1,58 @@
+package lesson_18;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest extends BaseTest {
+
+    @Test
+    @Description("Checking the addition of two numbers : 9 + 3 = 12")
+    @Story("Addition")
+    public void additionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"сложить\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "12");
+    }
+
+    @Test
+    @Description("Testing subtraction of two numbers : 9 - 3 = 6")
+    @Story("Subtraction")
+    public void subtractionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"вычесть\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "6");
+    }
+
+    @Test
+    @Description("Checking the multiplication of two numbers : 9 * 3 = 27")
+    @Story("Multiplication")
+    public void multiplicationTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"умножение\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "27");
+    }
+
+    @Test
+    @Description("Checking division of two numbers : 9 / 3 = 3")
+    @Story("Division")
+    public void divisionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"9\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"разделить\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"3\"]")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"равно\"]")).click();
+        String result = driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(result, "3");
+    }
+}
---
+++ AQA_Java/src/test/java/lesson_18/BaseTest.java
@@ -0,0 +1,38 @@
+package lesson_18;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    static AppiumDriver<AndroidElement> driver = null;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "HUAWEI MAR-LX1M API 29");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void quit() {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+}
